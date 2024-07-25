8c9e6a9dd1e367a8bb2b9aed78de1a3adb729cc2
Wolfmyths <tdellegrin@gmail.com>
2024-07-25 17:23:52 -0400

    Fix util

--- utils/update_qm_files.py
+++ utils/update_qm_files.py
@@ -5,16 +5,11 @@ import subprocess
 # Converts all .ts into .qm
 
 def update_qm_files() -> None:
-    files = glob.glob(os.path.join('.', 'translations', '*.ts'))
-    trans_bi_dir = os.path.join('.', 'src', 'lang')
-
-    print(os.listdir(trans_bi_dir))
+    files = glob.glob(os.path.join('translations', '*.ts'))
+    trans_bi_dir = os.path.join('src', 'lang')
 
     for file_name in files:
-        print('Converting ' + file_name)
-        file_name_bi = file_name[:-2] + 'qm'
+        file_name_bi = file_name.split('\\')[-1][:-2] + 'qm'
         subprocess.call(['pyside6-lrelease', file_name, '-qm', os.path.join(trans_bi_dir, file_name_bi)])
-    
-    print(os.listdir(trans_bi_dir))
 
 update_qm_files()

575d95ee7986de6259b6686429d65dd0973f9563
Wolfmyths <tdellegrin@gmail.com>
2024-07-25 16:59:14 -0400

    Debug stuff

--- utils/update_qm_files.py
+++ utils/update_qm_files.py
@@ -9,7 +9,10 @@ def update_qm_files() -> None:
     trans_bi_dir = os.path.join('.', 'src', 'lang')
 
     for file_name in files:
+        print('Converting ' + file_name)
         file_name_bi = file_name[:-2] + 'qm'
         subprocess.call(['pyside6-lrelease', file_name, '-qm', os.path.join(trans_bi_dir, file_name_bi)])
+    
+    print(os.listdir(trans_bi_dir))
 
 update_qm_files()

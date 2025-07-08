ed7a67e67cf93b5c5394378e7f9442bf99edfba4
Wolfmyths <tdellegrin@gmail.com>
2025-07-08 03:00:15 -0400

    Create createEXE.sh

---
+++ createEXE.sh
@@ -0,0 +1,13 @@
+echo Setting Variables
+disFolder="Myth Mod Manager"
+Exe="Myth Mod Manager.exe"
+Txt="requirements.txt"
+Spec="main.spec"
+
+echo Installing Dependencies
+pip install -r $Txt
+
+echo Running Pyinstaller
+pyinstaller --clean $Spec --distpath ./$disFolder
+
+echo Installation Finished!

5ec96460c837e5bed96c57d174e5628bb3d31216
Wolfmyths <tdellegrin@gmail.com>
2026-09-16 20:57:14 -0400

    For sure fixed the linux test

--- tests/objects/test_delete_mod.py
+++ tests/objects/test_delete_mod.py
@@ -1,11 +1,27 @@
 import os
+import platform
+import shutil
 
 import pytest
 
-from PySide6.QtCore import QFile
-
 from src.helpers.save_manager import Save
 from src.objects.delete_mod import DeleteMod
+
+class Mock_QFile:
+    def __init__(self, path: str) -> None:
+        self.path = path
+        #print("Mock QFile Created with the path:", path)
+    
+    def moveToTrash(self) -> bool:
+        #print("Moving to trash")
+        shutil.rmtree(self.path)
+        return not os.path.exists(self.path)
+    
+    def errorString(self) -> str:
+        return "Mock error string"
+    
+    def error(self) -> str:
+        return "Mock error"
 
 MOCK_MODS = (
     ('make game easy mod', True), 
@@ -15,8 +31,9 @@ MOCK_MODS = (
 
 @pytest.mark.parametrize(("mod_name", "disabled"), MOCK_MODS)
 def test_thread(monkeypatch: pytest.MonkeyPatch, create_mod_dirs: str, createTemp_Config_ini: str, createTemp_Mod_ini: str, mod_name: str, disabled: bool) -> None:  # pyright: ignore[reportUnusedParameter]
-    # QFile.moveToTrash doesn't work on tmp files on Linux
-    monkeypatch.setattr(QFile, "moveToTrash", os.remove)
+    if platform.system().startswith("Linux"):
+        # QFile.moveToTrash doesn't work on tmp files on Linux
+        monkeypatch.setattr("src.objects.delete_mod.QFile", Mock_QFile)
 
     Save(createTemp_Mod_ini)
     Save.setEnabled(mod_name, disabled)
